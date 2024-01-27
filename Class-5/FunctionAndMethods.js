64dd9f5e6dce7d38147cd355d77eab7189669d49
mohiqudraty <mohi.qudraty@gmail.com>
2024-01-27 14:58:18 +0600

    learned function methods like forEach, filter, map, reduce, etc

--- Class-5/FunctionAndMethods.js
+++ Class-5/FunctionAndMethods.js
@@ -61,3 +61,93 @@
 // Print Hello World!
 // const printHelloWorld = () => console.log("Hello World!");
 // printHelloWorld();
+
+// -foEach Loop in Arrays-----------------
+// ====================== || Return ❌
+
+// arr.forEach(callBackFunction)
+
+// callBackFunction: Here, it is a function to execute for each element in the array .
+
+// *A callback is a function passed as an argument to another function.
+
+// arr.forEach((val) => {
+//     console.log(val)
+// })
+
+// let arr = [1, 2, 3, 4, 5];
+
+// if we want to do anything for each Element of an array we use forEach function/method
+// arr.forEach(function (val) {
+//   // value at each idx
+//   console.log(val);
+// });
+
+// * forEach have 3 parameters (value, idx, array) // optional
+
+// -Some More Array Methods
+// ======================== || Return a New Array ✅
+
+// ----Map----------------------
+// -------
+// Crates a new array with the results of some operation. The value its callback returns are used to from new array.
+
+// arr.map(callBackFunction(value, idx, array)){}
+
+// let num = [10, 20, 30, 40, 50];
+
+// let newNum = num.map((value) => {
+//   return value ** 2; // square value
+// });
+// console.log(newNum);
+
+// ----Filter--------------------
+// ----------
+// Crates a new array of elements that give true for a condition/Filter.
+// Eg: all even elements;
+
+// let newArray = arr.filter( (value) => {
+//     return value % 2 === 0;
+// })
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let evenArr = num.filter((value) => {
+//   return value % 2 === 0;
+// });
+// console.log(evenArr);
+// let oddArr = num.filter((value) => {
+//   return value % 2 !== 0;
+// });
+// console.log(oddArr);
+
+// let conditionArr = num.filter((value) => {
+//   return value > 3 && value < 8;
+// });
+// console.log(conditionArr);
+
+// ----Reduce
+// ----------
+// Performs some operations & Reduces the array to a single value. It returns that single value.
+
+// reduce start from first 2 value
+// let arr = [1, 2, 3, 4, 5];
+
+// const output = arr.reduce((res, curr) => {
+//   return res + curr;
+// });
+// console.log(output); // 15
+
+// // find the largest number ----
+// let arr = [1, 2, 7, 4, 5];
+// const largestNumber = arr.reduce((prev, current) => {
+//   return prev > current ? prev : current;
+// });
+// console.log(largestNumber);
+
+// find the smallest number ----
+// let arr = [1, 2, 7, 4, 5];
+// const smallestNumber = arr.reduce((prev, current) => {
+//   return prev < current ? prev : current;
+// });
+// console.log(smallestNumber);
--- Class-5/Practices.js
+++ Class-5/Practices.js
@@ -1,6 +1,7 @@
 // Let's Practices
 // ---------------
-
+// -Function
+// ---------
 // Qs-1: Create a function using the "function" keyword that takes a String as an argument & returns the number of vowels in the string .
 
 // Answer-1: Print the vowels character
@@ -89,3 +90,57 @@
 // };
 // const result = countVowels("Hello World! How Are You");
 // console.log(result);
+
+// -forEach function/method
+// ------------------------
+//Qs-3: For a given numbers. print the square of each value using the forEach loop.
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.forEach((value) => {
+//   console.log(value * value);
+// });
+
+// different way
+// let squareNumber = (value) => {
+//   console.log(value * value);
+// };
+
+// arr.forEach(squareNumber); // callBack function---
+
+// Filter Method -----------
+// Qs-4: We are given Array of marks of students. Filter out of the marks of students that scored 90+.
+
+// const marks = [34, 65, 97, 89, 90, 98, 45, 95];
+
+// const topMarks = marks.filter((val) => {
+//   return val > 90;
+// });
+// console.log(topMarks); // output- [97, 98, 95]
+
+// Qs-5: Take a Number n as input from user . Create an array of numbers from 1 to n .
+
+// Use the reduce method to calculate sum of all numbers in the array .
+
+// ArrayUse the reduce method tho calculate product of all numbers n te array.
+
+// let userN = prompt("Enter a Number");
+
+// let arr = [];
+// for (let i = 1; i <= userN; i++) {
+//   arr[i - 1] = i; // 1(0), 2(1), 3(2), 4(3), 5(4)
+// }
+
+// console.log(arr);
+
+// sum of an array --------------------------
+// const newArray = arr.reduce((prev, current) => {
+//   return prev + current;
+// });
+// console.log("Sum Of User Array = ", newArray);
+
+// factorial-----------------------------
+// factorial of an array -------------multiply the all numbers of an array.
+// const factorial = arr.reduce((prev, current) => {
+//   return prev * current;
+// });
+// console.log("factorial Of User Array = ", factorial);
